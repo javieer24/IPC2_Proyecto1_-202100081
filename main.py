4ba5acf4a718b8f8a0fe0a06df30e55af81999e1
javieer24 <javiermonjesnuevo@gmail.com>
2023-08-20 19:30:31 -0600

    Se agregaron funciones y clases
    nodo, lista, reporte reportegrafico, def etc...

--- main.py
+++ main.py
@@ -1,56 +1,180 @@
 import xml.etree.ElementTree as ET
-import os
+from graphviz import Digraph
 
-# Menú
-def cargar_archivo():
-    # Aquí se puede implementar la lógica para cargar un archivo
+class Nodo:
+    """
+    Esta clase representa un nodo en una lista enlazada.
+    Atributos:
+        nombre: El nombre del nodo.
+        siguiente: Una referencia al siguiente nodo en la lista.
+    """
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.siguiente = None
+
+class Lista:
+    """
+    Esta clase representa una lista enlazada.
+    Atributos:
+        cabeza: Una referencia a la cabeza de la lista.
+    Métodos:
+        insertar(nombre): Inserta un nuevo nodo con el nombre dado al final de la lista.
+        buscar(nombre): Busca un nodo con el nombre dado en la lista y devuelve Verdadero si se encuentra, Falso en caso contrario.
+        generar_reporte_grafico(): Genera un informe gráfico utilizando Graphviz (no implementado).
+    """
+    def __init__(self):
+        self.cabeza = None
+
+    def insertar(self, nombre):
+        nuevo_nodo = Nodo(nombre)
+        if self.cabeza is None:
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente is not None:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+
+    def buscar(self, nombre):
+        actual = self.cabeza
+        while actual is not None:
+            if actual.nombre == nombre:
+                return True
+            actual = actual.siguiente
+        return False
+
+    def generar_reporte_grafico(self):
+        # Aquí puedes implementar la lógica para generar un informe gráfico utilizando Graphviz
+        pass
+
+class Reporte:
+    """
+    Esta clase representa un informe.
+    Métodos:
+        generar_reporte_grafico(): Genera un informe gráfico (no implementado).
+    """
+    def generar_reporte_grafico(self):
+        pass
+
+class ReporteGrafico(Reporte):
+    """
+    Esta clase representa un informe gráfico.
+    Métodos:
+        generar_reporte_grafico(): Genera un informe gráfico utilizando Graphviz (no implementado).
+    """
+    def generar_reporte_grafico(self):
+        # Implementación del informe gráfico utilizando Graphviz
+        pass
+
+class ReporteTabla(Reporte):
+    def generar_reporte_grafico(self):
+        # Implementación del reporte en forma de tabla
+        pass
+
+def cargar_archivo(ruta):
+    tree = ET.parse(ruta)
+    root = tree.getroot()
+    for senal in root.findall('senal'):
+        nombre = senal.get('nombre')
+        t = int(senal.get('t'))
+        A = int(senal.get('A'))
+        # Aquí se puede implementar la lógica para procesar la señal de audio
+        for dato in senal.findall('dato'):
+            t_dato = int(dato.get('t'))
+            A_dato = int(dato.get('A'))
+            # Aquí se puede implementar la lógica para procesar los datos de la señal de audio
+
+def guardar_datos():
+    # Aquí se puede implementar la lógica para guardar los datos ingresados por el usuario
     pass
+
+def generar_grafica(nombre, datos):
+    dot = Digraph()
+    # Aquí se puede implementar la lógica para agregar nodos y aristas a la gráfica utilizando los datos
+    dot.render(nombre + '.gv', view=True, format='png')
 
 def procesar_archivo():
-    # Aquí se puede implementar la lógica para procesar un archivo
-    pass
+    # Aquí puedes implementar la lógica para procesar el archivo cargado previamente
+    print("Calculando la matriz binaria...")
+    # Aquí puedes implementar la lógica para calcular la matriz binaria
+    print("Realizando suma de tuplas...")
+    # Aquí puedes implementar la lógica para realizar la suma de tuplas
 
 def escribir_archivo_salida():
-    # Aquí se puede implementar la lógica para escribir un archivo de salida
-    pass
+    # Aquí puedes implementar la lógica para escribir el archivo de salida
+    ruta = input("Ingrese la ruta donde desea guardar la imagen: ")
+    dot = Digraph()
+    dot.node('Prueba 1 reducida')
+    dot.node('5')
+    dot.node('7')
+    dot.node('0')
+    dot.node('6')
+    dot.node('0_2')
+    dot.node('9')
+    dot.node('4')
+    dot.edges([('Prueba 1 reducida', '5'), ('Prueba 1 reducida', '7'), ('Prueba 1 reducida', '0'), ('Prueba 1 reducida', '6'), ('Prueba 1 reducida', '0_2'), ('Prueba 1 reducida', '9'), ('Prueba 1 reducida', '4')])
+    dot.edge('Prueba 1 reducida', '5', label='g=1(t-1.3)')
+    dot.edge('Prueba 1 reducida', '7', label='g=2(t-2.5)')
+    # Aquí puedes agregar más nodos y aristas a la gráfica
+    dot.render(ruta, view=True, format='png')
 
 def mostrar_datos_estudiante():
-    # Aquí se puede implementar la lógica para mostrar los datos del estudiante
-    pass
+    # Aquí puedes implementar la lógica para mostrar los datos del estudiante
+    print("Nombre del estudiante:", nombre_estudiante)
 
 def generar_grafica():
-    # Aquí se puede implementar la lógica para generar una gráfica
-    pass
+    # Aquí puedes implementar la lógica para generar una gráfica utilizando Graphviz
+    dot = Digraph()
+    dot.node('Prueba 1 reducida')
+    dot.node('5')
+    dot.node('7')
+    dot.node('0')
+    dot.node('6')
+    dot.node('0_2')
+    dot.node('9')
+    dot.node('4')
+    dot.edges([('Prueba 1 reducida', '5'), ('Prueba 1 reducida', '7'), ('Prueba 1 reducida', '0'), ('Prueba 1 reducida', '6'), ('Prueba 1 reducida', '0_2'), ('Prueba 1 reducida', '9'), ('Prueba 1 reducida', '4')])
+    dot.edge('Prueba 1 reducida', '5', label='g=1(t-1.3)')
+    dot.edge('Prueba 1 reducida', '7', label='g=2(t-2.5)')
+    # Aquí puedes agregar más nodos y aristas a la gráfica
+    dot.render('prueba.gv', view=True, format='png')
 
 def inicializar_sistema():
-    # Aquí se puede implementar la lógica para inicializar el sistema
+    # Aquí puedes implementar la lógica para inicializar el sistema
     pass
 
-while True:
-    print("Menú principal:")
-    print("1. Cargar archivo")
-    print("2. Procesar archivo")
-    print("3. Escribir archivo salida")
-    print("4. Mostrar datos del estudiante")
-    print("5. Generar gráfica")
-    print("6. Inicializar sistema")
-    print("7. Salida")
+def main():
+    """
+    Esta función es el punto de entrada del programa y muestra un menú al usuario para interactuar con el sistema.
+    El menú permite al usuario cargar un archivo, procesar el archivo, escribir un archivo de salida, mostrar datos del estudiante, generar una gráfica, inicializar el sistema y salir del programa.
+    """
+    while True:
+        print("Menú principal:")
+        print("1. Cargar archivo")
+        print("2. Procesar archivo")
+        print("3. Escribir archivo salida")
+        print("4. Mostrar datos del estudiante")
+        print("5. Generar gráfica")
+        print("6. Inicializar sistema")
+        print("7. Salida")
 
-    opcion = input("Ingrese una opción: ")
+        opcion = input("Ingrese una opción: ")
 
-    if opcion == "1":
-        cargar_archivo()
-    elif opcion == "2":
-        procesar_archivo()
-    elif opcion == "3":
-        escribir_archivo_salida()
-    elif opcion == "4":
-        mostrar_datos_estudiante()
-    elif opcion == "5":
-        generar_grafica()
-    elif opcion == "6":
-        inicializar_sistema()
-    elif opcion == "7":
-        break
-    
+        if opcion == "1":
+            ruta = input("Ingrese la ruta del archivo: ")
+            cargar_archivo(ruta)
+        elif opcion == "2":
+            procesar_archivo()
+        elif opcion == "3":
+            escribir_archivo_salida()
+        elif opcion == "4":
+            mostrar_datos_estudiante()
+        elif opcion == "5":
+            generar_grafica()
+        elif opcion == "6":
+            inicializar_sistema()
+        elif opcion == "7":
+            break
 
+if __name__ == "__main__":
+    main()
